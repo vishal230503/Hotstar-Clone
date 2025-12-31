3928e01c98df33caa81ccaedcf2faf2c38fc25af
Vishal Narkhede <104969449+vishal230503@users.noreply.github.com>
2025-12-31 15:59:44 +0530

    Update main.tf

--- EKS_TERRAFORM/main.tf
+++ EKS_TERRAFORM/main.tf
@@ -17,8 +17,8 @@ resource "aws_iam_role" "example" {
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSClusterPolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
-  role       = aws_iam_role.example.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = arn:aws:iam::850874729072:role/Mario
 }
 
 #get vpc data
@@ -35,7 +35,7 @@ data "aws_subnets" "public" {
 #cluster provision
 resource "aws_eks_cluster" "example" {
   name     = "EKS_CLOUD"
-  role_arn = aws_iam_role.example.arn
+  role_arn = arn:aws:iam::850874729072:role/Mario
 
   vpc_config {
     subnet_ids = data.aws_subnets.public.ids
@@ -65,17 +65,17 @@ resource "aws_iam_role" "example1" {
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example1.name
+  role       = arn:aws:iam::850874729072:role/Mario
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example1.name
+  role       = arn:aws:iam::850874729072:role/Mario
 }
 
 resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example1.name
+  role       = arn:aws:iam::850874729072:role/Mario
 }
 
 #create node group
